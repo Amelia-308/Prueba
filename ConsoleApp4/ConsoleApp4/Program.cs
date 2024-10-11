b4baae5dbd6b6a47ed6117d8caa95407aabbd08a
Amelia-308 <ameliavi@iesflorenciopintadoo.es>
2024-10-11 19:29:46 +0200

    ejercicio 4

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+                char opcion;
+                Console.WriteLine("a. Sumar");
+                Console.WriteLine("b. Restar");
+                Console.WriteLine("c. Salir");
+                opcion = char.Parse(Console.ReadLine());
+
+                if (opcion == 'a')
+                {
+                    Console.WriteLine("Has escogido SUMAR");
+                }
+                else if (opcion == 'b')
+                {
+                    Console.WriteLine("Has escogido RESTAR");
+                }
+                else
+                {
+                    Console.WriteLine("SALIR");
+                }
+
+            }
+        }
+    };
+       
